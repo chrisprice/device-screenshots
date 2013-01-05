0fbfdb0d58c967c50d791a9fb532c83aa860afbd
Chris Price <price.c@gmail.com>
2013-01-05 23:56:57 +0000

    always load transform source points from screenshot image

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 define(["renderer", "projection-solver", "sylvester"], function(Renderer, solveProjection, sylvester) {
 
 
-  var a1 = [0, 1136-0];
+  var a1 = [0, 1];
   var a2 = [404, 1848-650];
 
-  var b1 = [640, 1136-0];
+  var b1 = [1, 1];
   var b2 = [896, 1848-354];
 
-  var c1 = [0, 1136-1136];
+  var c1 = [0, 0];
   var c2 = [1219, 1848-1293];
 
-  var d1 = [640, 1136-1136];
+  var d1 = [1, 0];
   var d2 = [1721, 1848-910];
 
   var canvas = document.getElementById('scene');
@@ -23,12 +23,24 @@ define(["renderer", "projection-solver", "sylvester"], function(Renderer, solveP
   };
   background.src = "8074156745_3fc93aa1cf_o.jpg";
 
-  var screenshot = new Image();
-  screenshot.onload = function() {
-    renderer.screenshotImage = screenshot;
-    updateTransformMatrix();
-  };
-  screenshot.src = "SearchResults.png";
+  function setScreenshot(src) {
+    var screenshot = new Image();
+    screenshot.onload = function() {
+      renderer.screenshotImage = screenshot;
+      a1[0] = 0;
+      a1[1] = screenshot.height;
+      b1[0] = screenshot.width;
+      b1[1] = screenshot.height;
+      c1[0] = 0;
+      c1[1] = 0;
+      d1[0] = screenshot.width;
+      d1[1] = 0;
+      updateTransformMatrix();
+    };
+    screenshot.src = src;
+  }
+
+  setScreenshot("SearchResults.png");
 
 
   function updateTransformMatrix() {
@@ -43,7 +55,7 @@ define(["renderer", "projection-solver", "sylvester"], function(Renderer, solveP
       return;
     }
 
-    var clickVector = sylvester.V([e.offsetX, 1848-e.offsetY]);
+    var clickVector = sylvester.V([e.offsetX, canvas.height-e.offsetY]);
     if (!currentPoint) {
       var minDistance = 100;
       [a2, b2, c2, d2].forEach(function(point) {
@@ -89,20 +101,7 @@ define(["renderer", "projection-solver", "sylvester"], function(Renderer, solveP
       alert('Error code: ' + e.target.error.code);
     };
     reader.onload = function(evt) {
-      var screenshot = new Image();
-      screenshot.onload = function() {
-        renderer.screenshotImage = screenshot;
-        a1[0] = 0;
-        a1[1] = screenshot.height;
-        b1[0] = screenshot.width;
-        b1[1] = screenshot.height;
-        c1[0] = 0;
-        c1[1] = 0;
-        d1[0] = screenshot.width;
-        d1[1] = 0;
-        updateTransformMatrix();
-      };
-      screenshot.src = evt.target.result
+      setScreenshot(evt.target.result)
     };
     reader.readAsDataURL(screenshotFile);
   });
